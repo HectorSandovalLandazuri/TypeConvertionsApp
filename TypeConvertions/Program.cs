263b3196aad6bd092ebee3d37bd170a0e54e655a
HectorSandovalLandazuri <88155761+HectorSandovalLandazuri@users.noreply.github.com>
2023-07-17 12:47:19 -0600

    Agregar archivos de proyecto.

---
+++ TypeConvertions/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+Console.Write("What is your Age: ");
+string? ageText = Console.ReadLine();
+
+bool isValidInt =int.TryParse(ageText,out int age);
+
+Console.WriteLine($"This is valid: {isValidInt}");
+if (isValidInt)
+{
+    Console.WriteLine(age + 15);
+}
+
+double testDouble = age;
+decimal testDecimal = (decimal) testDouble;
